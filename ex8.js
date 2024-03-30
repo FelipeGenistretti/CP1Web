97a4d435b7870f07500f1ff2cedcb5a37f50699c
Felipe Genistretti Rodrigues <genistrettifelipe@gmail.com>
2024-03-30 00:24:36 -0300

    Resolução exercício 8 realizado por Renan Simões RM555584.GitHub: https://github.com/rrrrnnnan

---
+++ ex8.js
@@ -0,0 +1,15 @@
+let texto = "Tecnologia é um produto da ciência e da engenharia que envolve um conjunto de instrumentos, métodos e técnicas que visam a resolução de problemas.";
+
+
+// 1) Primeira posição de "em"
+let primeiraPosicaoEm = texto.indexOf("em");
+console.log("1) 'em':", primeiraPosicaoEm);
+
+
+// 2) Última posição de "ia"
+let ultimaPosicaoIa = texto.lastIndexOf("ia");
+console.log("2) 'ia':", ultimaPosicaoIa);
+
+
+// 3) Palavra "ciência"
+let posicaoCiencia = texto.indexOf("ciência");
