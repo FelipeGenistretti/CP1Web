c7cd1e0a3691fe67156d52d29fbcc12a7ccea290
Felipe Genistretti Rodrigues <genistrettifelipe@gmail.com>
2024-03-29 23:57:26 -0300

    Resolução exercício 2 realizado por Gabriel Guilherme RM558638.GitHub:https://github.com/Manerrando28

---
+++ ex2.js
@@ -0,0 +1,16 @@
+// USANDO !=
+let num1 = 10;
+let num2 = 15;
+console.log(num1 != num2);
+
+
+// USANDO ==
+let num3 = 20;
+let num4 = 20;
+console.log(num3 == num4);
+
+
+//USANDO >=
+let num5 = 30;
+let num6 = 25;
+console.log(num5 >= num6);
